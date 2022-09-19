7d3639f4b526c080686c6d844a055b0ec997e5ca
liaojiao <15727049225@163.com>
2022-09-19 15:57:23 +0800

    sss

--- interface_testCase/test_001 通过在方法中传参来加前后置固件.py
+++ interface_testCase/test_001 通过在方法中传参来加前后置固件.py
@@ -2,6 +2,6 @@ import requests as requests
 
 
 class TestLogin:
-    def test_aaa(self, qianzhi_1):  # 在方法里面加前置方法，就是把前后置固件方法名传参进来
+    def test_aaa(self, qianzhi_1):  # 通过在方法中传参来加前后置固件
         res = requests.post(r"http://tcc.taobao.com/cc/json/mobile_tel_segment.htm", data="tel=15727049225")
         print(res.status_code)
