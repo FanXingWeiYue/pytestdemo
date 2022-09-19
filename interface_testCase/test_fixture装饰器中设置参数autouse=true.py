bb8a0695076e5d798811f00302d4f28d33ad36cf
liaojiao <15727049225@163.com>
2022-09-19 15:53:25 +0800

    sss

--- interface_testCase/test_fixture装饰器中设置参数autouse=true.py
+++ interface_testCase/test_fixture装饰器中设置参数autouse=true.py
@@ -5,8 +5,7 @@ from common.yamlutil import read_yaml
 
 
 class TestLogin:
-    @pytest.mark.run(order=1)
-    def test_aaa(self):  # 该方法虽然没有引用任何
+    def test_aaa(self):  # 该方法虽然传前后置固件，但是qianzhi_2固件设置的autouse=true，所以会自动执行
         res = requests.post(r"http://tcc.taobao.com/cc/json/mobile_tel_segment.htm", data="tel=15727049225")
         print(res.status_code)
 
