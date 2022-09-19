753a3fa8c130dd5383541e97f1287f03b9438ea9
liaojiao <15727049225@163.com>
2022-09-19 15:54:37 +0800

    sss

--- interface_testCase/test_002 fixture装饰器中设置参数autouse=true.py
+++ interface_testCase/test_002 fixture装饰器中设置参数autouse=true.py
@@ -1,11 +1,7 @@
-import pytest
 import requests as requests
-
-from common.yamlutil import read_yaml
 
 
 class TestLogin:
     def test_aaa(self):  # 该方法虽然传前后置固件，但是qianzhi_2固件设置的autouse=true，所以会自动执行
         res = requests.post(r"http://tcc.taobao.com/cc/json/mobile_tel_segment.htm", data="tel=15727049225")
         print(res.status_code)
-
---
+++ interface_testCase/test_在类申明上加usefixtures装饰器.py
@@ -0,0 +1,17 @@
+import pytest
+import requests as requests
+
+from common.yamlutil import read_yaml
+
+
+@pytest.mark.usefixtures("qianzhi_1")  # 在类声明上面加 @pytest.mark.usefixtures() ，代表这个类里面所有测试用例都会调用该fixture
+class TestLogin:
+    @pytest.mark.run(order=1)
+    def test_aaa(self, qianzhi_2):  # 在方法里面加前置方法，就是把前置方法名传参进来
+        res = requests.post(r"http://tcc.taobao.com/cc/json/mobile_tel_segment.htm", data="tel=15727049225")
+        print(res.status_code)
+
+    # @pytest.mark.run(order=2)
+    # def test_bbb(self):
+    #     value = read_yaml("./hh.yaml")
+    #     print(value)
